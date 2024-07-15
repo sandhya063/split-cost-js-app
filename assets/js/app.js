ac4beec7da3cab135e7a4d247e71663d7aff645f
sandhya063 <165256047+sandhya063@users.noreply.github.com>
2024-07-15 21:19:18 +1000

    made changes of split-cpst-js-app

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,19 @@
-console.log(sandhya)
+function User(name, email, mobile, photo) {
+    this.name = name;
+    this.email= email;
+    this.mobile = mobile;
+    this.photo = photo;
+}
+
+function Expense(description, amount) {
+    this.description = description;
+    this.amount = amount;
+    this.isSettled = false;
+    this.date = new Date();
+}
+function SplitCostApp() {
+    this.unsettledAmount = 0;
+    this.users = [];
+    this.expenses = [];
+
+}
